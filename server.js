0f362ada065fd99d1f467cbc092003d0eb9ed992
Mac Van Anh <lamhaianh@gmail.com>
2013-06-28 19:13:18 +0700

    Init server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.set(express.bodyParser());
+	app.use(require('stylus').middleware({src: __dirname + '/public' }));
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res) {
+
+	res.render('layout', 
+		{
+			title: 'Home'
+		}
+	);	
+
+});
+
+app.listen(port);
+console.log('Server is listening on port ' + port);
