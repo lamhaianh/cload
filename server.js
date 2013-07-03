9431b34726da57d4f735b59575217496ed785a65
Mac Van Anh <lamhaianh@gmail.com>
2013-07-03 10:37:34 +0700

    add register page

--- server.js
+++ server.js
@@ -28,5 +28,9 @@ app.get('/login', function(req, res) {
 	res.render('login', {})
 });
 
+app.get('/register', function(req, res) {
+	res.render('register', {})
+});
+
 app.listen(port);
 console.log('Server is listening on port ' + port);
